f5dd786cf03e8764226d7ec04a737829ab70678f
Pavlo Marchenko <pmarchenko@griddynamics.com>
2025-04-08 09:59:45 +0200

    add WIF mode for GCP

--- src/common/env_setup.py
+++ src/common/env_setup.py
@@ -11,6 +11,7 @@ load_dotenv()
 
 GCP_REGION = os.getenv("GCP_REGION", "us-central1")
 GCP_PROJECT = os.getenv("GCP_PROJECT", "gd-gcp-rnd-analytical-platform")
+GCP_MODE = os.getenv("GCP_MODE", "LOCAL")
 
 logging.basicConfig(
     format=f"%(asctime)s: %(levelname)s - %(message)s",
@@ -22,19 +23,22 @@ logger = logging.getLogger(__name__)
 
 
 def setup_aiplatform():
-    GOOGLE_APPLICATION_CREDENTIALS = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
-    if json.loads(GOOGLE_APPLICATION_CREDENTIALS)["type"] == "authorized_user":
-        with open("credentials.json", "w") as file:
-            file.write(GOOGLE_APPLICATION_CREDENTIALS)  # dev env only
-        os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "credentials.json"
+    if GCP_MODE == "WIF":
         aiplatform.init(location=GCP_REGION, project=GCP_PROJECT)
-        return None
     else:
-        credentials = service_account.Credentials.from_service_account_info(
-            json.loads(GOOGLE_APPLICATION_CREDENTIALS)
-        )
-        aiplatform.init(location=GCP_REGION, project=GCP_PROJECT, credentials=credentials)
-        return credentials
+        GOOGLE_APPLICATION_CREDENTIALS = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
+        if json.loads(GOOGLE_APPLICATION_CREDENTIALS)["type"] == "authorized_user":
+            with open("credentials.json", "w") as file:
+                file.write(GOOGLE_APPLICATION_CREDENTIALS)  # dev env only
+            os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "credentials.json"
+            aiplatform.init(location=GCP_REGION, project=GCP_PROJECT)
+            return None
+        else:
+            credentials = service_account.Credentials.from_service_account_info(
+                json.loads(GOOGLE_APPLICATION_CREDENTIALS)
+            )
+            aiplatform.init(location=GCP_REGION, project=GCP_PROJECT, credentials=credentials)
+            return credentials
 
 
 def create_llm(model_name="gemini-2.0-flash", temperature=1.0):
